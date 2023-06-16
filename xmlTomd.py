481b3f5e957a9a3110b46accb2e22e9ebf7950a4
Paul Gilzow <paul.gilzow@platform.sh>
2023-06-16 12:04:50 -0500

    rewrite to handle our linkchecker report xml doc

--- xmlTomd.py
+++ xmlTomd.py
@@ -2,41 +2,37 @@ import xml.etree.ElementTree as ET
 from tabulate import tabulate
 import argparse
 
+
 def parse_nmap_xml(xmlfile):
     # Parse XML
     tree = ET.parse(xmlfile)
     root = tree.getroot()
 
-    for host in root.findall('host'):
-        # Get IP address
-        ip = host.find('address').get('addr')
-        print(f"## {ip}\n")
+    tableData = []
+    for link in root.findall('urldata'):
+        # we need
+        # * broken link
+        # * the URL of where the broken link appears
+        # * where in the above it appears
+        # * the "result"
+        fullURL = link.find('realurl').text
+        parentLink = link.find("parent")
+        parentURL = parentLink.text
+        parentLoc = parentLink.attrib.get('line')
+        result = link.find('valid').attrib.get('result')
 
-        # Prepare data for table
-        table_data = []
-        for port in host.iter('port'):
-            # Get port details
-            port_id = f"{port.get('portid')}/{port.get('protocol')}"
-            service = port.find('service').get('name')
-            product = port.find('service').get('product')
-            version = port.find('service').get('version')
-            extrainfo = port.find('service').get('extrainfo')
+        brokenRow = [fullURL, parentURL, parentLoc, result]
+        tableData.append(brokenRow)
 
-            # Combine all service details and filter out None values
-            service_info_parts = [service, product, version, extrainfo]
-            service_info = " ".join(part for part in service_info_parts if part)
+    # Create and print table
+    table = tabulate(tableData, headers=['Broken Link', 'Parent', 'Line in Parent', 'Reason'], tablefmt="github")
+    print(table)
+    print("\n")
 
-            table_data.append([port_id, service_info])
-        
-        # Create and print table
-        table = tabulate(table_data, headers=['Port', 'Service'], tablefmt="pipe")
-        print(table)
-        print("\n")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Parse an Nmap XML file.')
     parser.add_argument('xmlfile', type=str, help='Path to the XML file')
     args = parser.parse_args()
-    
-    parse_nmap_xml(args.xmlfile)
 
+    parse_nmap_xml(args.xmlfile)
